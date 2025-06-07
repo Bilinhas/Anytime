080caa5423c710d48b2aa7375e4e65431c5c7053
Bilinhas <bilinhas.17@gmail.com>
2025-06-07 18:35:47 -0300

    ver 1.0.0

--- Anytime ver 1.0.0/app.js
+++ Anytime ver 1.0.0/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/bands',bandsRouter);
-app.use('/user/login', loginRouter);
+app.use('/login', loginRouter);
 app.use('/songs', songRouter);
 
 // catch 404 and forward to error handler
